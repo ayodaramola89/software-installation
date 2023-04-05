374f5dbe3251c93851f1662c7844869e69ef76cc
ayodaramola89 <105969074+ayodaramola89@users.noreply.github.com>
2023-04-05 11:13:03 +0000

    Tomcat-Ubuntu-Installation.sh
    Tomcat-Ubuntu-Installation.sh

---
+++ tomcat-install-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install tomcat9
+sudo apt install tomcat9
+sudo systemctl start tomcat9
+sudo systemctl enable tomcat9
+echo "That completes Tomcat Installation"
+ 
